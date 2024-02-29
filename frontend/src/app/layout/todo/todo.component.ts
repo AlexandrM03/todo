3016e86f8818fd76fb2e82c69135290d312e0a27
AlexandrM03 <alexandrmozolevskiy@gmail.com>
2024-02-29 14:21:21 +0300

    animations

--- frontend/src/app/layout/todo/todo.component.ts
+++ frontend/src/app/layout/todo/todo.component.ts
@@ -7,17 +7,37 @@ import { TodoCardComponent } from "../../components/todo-card/todo-card.componen
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
     selector: 'app-todo',
     standalone: true,
     templateUrl: './todo.component.html',
     styleUrl: './todo.component.css',
-    imports: [FormsModule, CommonModule, TodoCardComponent, MatInputModule, MatButtonModule, MatFormFieldModule]
+    imports: [
+        FormsModule,
+        CommonModule,
+        TodoCardComponent,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        CdkDropListGroup,
+        CdkDropList,
+        CdkDrag
+    ],
+    animations: [
+        trigger('scaleInOut', [
+            transition(':enter', [
+                style({ transform: 'scale(0)' }),
+                animate('0.2s', style({ transform: 'scale(1)' })),
+            ])
+        ]),
+    ]
 })
 export class TodoComponent implements OnInit {
-    todos: Todo[] = [];
+    uncompletedTodos: Todo[] = [];
+    completedTodos: Todo[] = [];
     newTodoTitle: string = '';
 
     constructor(
@@ -30,7 +50,15 @@ export class TodoComponent implements OnInit {
 
     loadTodos(): void {
         this.todoService.getAllTodos().subscribe({
-            next: (todos: Todo[]) => this.todos = todos,
+            next: (todos: Todo[]) => {
+                todos.forEach(todo => {
+                    if (todo.completed) {
+                        this.completedTodos.push(todo);
+                    } else {
+                        this.uncompletedTodos.push(todo);
+                    }
+                });
+            },
             error: (error: any) => console.error(error)
         });
     }
@@ -46,7 +74,7 @@ export class TodoComponent implements OnInit {
         if (this.newTodoTitle.trim()) {
             this.todoService.createTodo(this.newTodoTitle.trim()).subscribe({
                 next: (todo: Todo) => {
-                    this.todos.push(todo);
+                    this.uncompletedTodos.push(todo);
                     this.newTodoTitle = '';
                 },
                 error: (error: any) => console.error(error)
@@ -55,10 +83,15 @@ export class TodoComponent implements OnInit {
     }
 
     updateTodoStatus(todo: Todo): void {
+        todo.completed = !todo.completed;
+        if (todo.completed) {
+            this.uncompletedTodos = this.uncompletedTodos.filter(t => t.id !== todo.id);
+            this.completedTodos.push(todo);
+        } else {
+            this.completedTodos = this.completedTodos.filter(t => t.id !== todo.id);
+            this.uncompletedTodos.push(todo);
+        }
         this.todoService.changeTodoStatus(todo.id).subscribe({
-            next: (updatedTodo: Todo) => {
-                todo.completed = updatedTodo.completed;
-            },
             error: (error: any) => console.error(error)
         });
     }
@@ -66,17 +99,28 @@ export class TodoComponent implements OnInit {
     deleteTodo(todo: Todo): void {
         this.todoService.deleteTodo(todo.id).subscribe({
             next: () => {
-                this.todos = this.todos.filter(t => t.id !== todo.id);
+                this.uncompletedTodos = this.uncompletedTodos.filter(t => t.id !== todo.id);
+                this.completedTodos = this.completedTodos.filter(t => t.id !== todo.id);
             },
             error: (error: any) => console.error(error)
         });
     }
 
-    getUncompletedTodos(): Todo[] {
-        return this.todos.filter(todo => !todo.completed);
-    }
+    drop(event: CdkDragDrop<Todo[]>): void {
+        const todo = event.item.data;
+        const previousContainer = event.previousContainer;
+        const currentContainer = event.container;
 
-    getCompletedTodos(): Todo[] {
-        return this.todos.filter(todo => todo.completed);
+        if (previousContainer !== currentContainer) {
+            transferArrayItem(previousContainer.data, currentContainer.data, event.previousIndex, event.currentIndex);
+            // this.updateTodoStatus(todo);
+            todo.completed = !todo.completed;
+            this.todoService.changeTodoStatus(todo.id).subscribe({
+                error: (error: any) => console.error(error)
+            });
+
+        } else {
+            moveItemInArray(currentContainer.data, event.previousIndex, event.currentIndex);
+        }
     }
 }
